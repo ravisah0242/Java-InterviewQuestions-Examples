e19a05e037b68ad90dc2fb6659547043387ad48c
Ravi <Ravi@DESKTOP-9P1MDVM>
2021-02-25 12:04:36 +0530

    vg

--- src/Ravi/ArrayListSortandCostomizeSoet.java
+++ src/Ravi/ArrayListSortandCostomizeSoet.java
@@ -1,4 +1,4 @@
-package zz;
+package Ravi;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -6,7 +6,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class Test {
+public class ArrayListSortandCostomizeSoet {
 
 	public static <T> void main(String[] args) {
 
